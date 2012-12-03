6da2594115d8af876bbe1c4cce6e6b9a82b9d6a1
Don Wang <devplus2010@gmail.com>
2012-12-03 00:08:13 -0500

    first commit

---
+++ library.php
@@ -0,0 +1,97 @@
+<?php
+/*///////////////////////////////////////////////////////////////////////////////////////////////////////////
+File                   : library.php
+Subject Code           : INT322A
+Date Submitted         : Oct. 17, 2012
+Purpose                : This program includes three functions to print out html header, menu bar, and footer.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+class DbLink {
+	private $link;
+	private $lines;
+	private $user;
+	private $pw;
+	private $host;
+	
+	public function __construct ($database_name) {
+		$this->lines = file("/home/int322_123a20/secret/topsecret");
+		$this->user = trim($this->lines[0]);
+		$this->pw = trim($this->lines[1]);
+		$this->host = trim($this->lines[2]);
+
+		$this->link = mysql_connect ($this->host, $this->user, $this->pw);
+		mysql_select_db ($database_name, $this->link);
+	}
+	
+	public function query ($sql_query) {
+		$result = mysql_query ($sql_query, $this->link);
+		return $result;
+	}
+	
+	public function emptyResult($sql_query) {
+		$result = $this->query($sql_query) or die('error: ' . mysql_error());
+		if(!result) {
+			$rtn = true;
+		}
+		else {
+			$row = mysql_fetch_assoc($result);
+			if(!$row) {
+				$rtn = true;
+			} else {
+				$rtn = false;
+			}
+		}
+		return $rtn;
+	}
+	
+	public function __destruct() {
+		mysql_close ($this->link);
+	}
+}
+
+class Menu {
+
+	private $menuStr;
+
+	public function __construct($items) {
+
+		$this->menuStr = "\n\t<div class='menu'>\n\t\t<ul>\n\t\t\t<li>\n";
+		foreach($items as $item) {
+			$this->menuStr .= "\t\t\t\t{$item}\n";
+		}
+		$this->menuStr .=  "\t\t\t</li>\n\t\t</ul>\n\t</div>\n";
+	}
+	
+	function displayMenu() {
+		print $this->menuStr;
+	}
+}
+
+// This function prints out html header
+function html_header($title) {
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title><?php print $title; ?></title>
+    <link rel="stylesheet" href="css/reset.css"   type="text/css" media="screen" /> 
+    <link rel="stylesheet" href="css/lab6.css" type="text/css" media="screen" /> 
+</head>
+<body>
+<?php
+}
+
+// This function prints out html footer
+function footer() {
+
+//	  date_default_timezone_set('America/Toronto');
+//    print "\t<div class='footer'>\n";
+//    copy right and time 
+//    print ("\t\t<p>This page &copy;2012 generated on " . date ("D d M Y H:i:s") . " by PHP</p>\n");
+?>
+	</div>
+</body>
+</html>
+<?php
+}
+?>
---
+++ login-crypt.php
@@ -0,0 +1,120 @@
+<?php
+//////////////////////////////////////////////////////////
+//  int322 lab6-login, crypted password, oo programming //
+//////////////////////////////////////////////////////////
+session_start();
+
+include 'library.php';
+
+$items = array ('<a href="login-crypt.php"><span>Lab6</span></a>', 
+				'<a href="#"><span>Item1</span></a>', 
+				'<a href="#"><span>Item2</span></a>',
+				'<a href="logout.php"><span>Logout</span></a>');
+
+if ($_GET['forgot'] == "forgot") {
+	html_header("Retrieve Password");
+	$myMenu = new Menu($items);
+	$myMenu->displayMenu();
+?>
+	<br><br>
+	<form method="POST" action="email.php">
+		<div class="login_t"><h1>Retrieve password</h1></div>
+		<div class="login_b">
+			<p>Enter your username</p>
+			<input type="text" name="username" value="<?php print $username; ?>" size="55"><br /><br />
+			<input type="submit" name="submit" value="Submit">
+		</div>
+	</form>
+	<br>
+<?php
+	footer();
+}
+else {
+	$username 		= "";
+	$password 		= "";
+	$loginErr 		= "";
+	$valid			= true;
+
+	if ($_POST) {
+		$username = strtolower(trim(htmlentities($_POST['username'])));
+		$password = trim(htmlentities($_POST['password']));
+
+		// validate user input using regular expression
+		if ( !preg_match("/^\w{4,}$/", $username) )  {
+			$loginErr = "Incorrect username or password";
+			$valid = false;
+		}
+		if ( !preg_match("/^[\w\d!@#\$%\^\&\*\(\)-\+\=\~]{4,32}$/", $password) )  {
+			$loginErr = "Incorrect username or password";
+			$valid = false;
+		}
+		
+		if ($valid) {
+			//Create connection to the mysql server
+			$lines = file("/home/int322_123a20/secret/topsecret");
+			$db = trim($lines[3]);
+			
+			//Create connection to the mysql server
+			$myLink = new DbLink($db);
+
+			$sql = "select username, password from users where username = '{$username}'";
+			
+			if ($result = $myLink->query($sql)) {
+			
+				if($row = mysql_fetch_assoc($result)){
+					$pswd=$row['password'];
+					$salt=substr($pswd, 0, 12);
+					if (crypt($password, $salt) == $row['password']) {
+					session_regenerate_id(); 
+					$_SESSION['username'] = $username;
+					}
+					else {
+						$loginErr = "Incorrect username or password";
+					}
+				}
+				else {
+					$loginErr = "Incorrect username or password";
+				}
+			}
+			else {
+				$loginErr = "Incorrect username or password";
+			}
+
+			// Free resultset
+			mysql_free_result($result);		
+		}
+	}
+
+	if (isset($_SESSION['username'])) {
+		html_header("logged in");
+		$myMenu = new Menu($items);
+		$myMenu->displayMenu();
+?>
+	<p>Welcome - you are already logged in.</p>
+	<a href="login-crypt.php?logout=logout" >Logout</a>
+<?php
+		footer();
+	}
+	else {
+		html_header("login");
+		$myMenu = new Menu($items);
+		$myMenu->displayMenu();
+?>
+	<br><br>
+	<form method="POST" action="">
+		<?php if ($loginErr)  print '<div class="loginErr"><h1>' . $loginErr . '</h1></div>' ; ?>
+		<div class="login_t"><h1>Log in</h1></div>
+		<div class="login_b">
+			<p>User Name</p>
+			<input type="text" name="username" value="<?php print $username; ?>" size="55"><br /><br />
+			<p>Password <a href="login-crypt.php?forgot=forgot">(Forgot password)</a></p>
+			<input type="text" name="password" value="<?php print $password; ?>" size="55"><br /><br />
+			<input type="submit" name="submit" value="Submit">
+		</div>
+	</form>
+	<br>
+<?php
+		footer();
+	}
+}
+?>
